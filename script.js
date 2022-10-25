f39afb2b1a349c1e17a227990d6b0068569f0946
Zeljko Culibrk <zeljkoculibrk7@gmail.com>
2022-10-25 21:27:41 +0200

    Add function that plays one round and game

--- script.js
+++ script.js
@@ -13,5 +13,66 @@ function getComputerChoice(){
         return "Rock";
     }
     else return "Something went wrong.";
-    
+}
+function playerChoose(){
+    let player = prompt("Please enter your sign:");
+    if(player.localeCompare("Rock", 'en', { sensitivity: 'base' })==0)
+         player = "Rock";
+    else if(player.localeCompare("Paper", 'en', { sensitivity: 'base' })==0)
+        player = "Paper";
+    else if(player.localeCompare("Scissors", 'en', { sensitivity: 'base' })==0)
+        player = "Scissors";
+    else
+        return console.log("You entered wrong value.");
+    return player;
+}
+
+function playRound(playerSelection, computerSelection){
+    let player = playerSelection;
+    console.log("player: " + player + " computerSelection: " + computerSelection);
+    if(player=="Rock" && computerSelection =="Paper")
+    {
+        console.log("You Lose! Paper beats Rock");
+        return 2;
+    }
+    else if(player == "Rock" && computerSelection == "Scissors")
+    {
+        console.log("You Win! Rock beats Scissors");
+        return 1;
+    }
+    else if(player == "Scissors" && computerSelection == "Rock")
+    {
+        console.log("You Lose! Rock beats Scissors");
+        return 2;
+    }
+    else if(player == "Scissors" && computerSelection == "Paper")
+    {
+        console.log("You Win! Scissors beat Paper");
+        return 1;
+    }
+    else if(player == "Paper" && computerSelection == "Rock")
+    {
+        console.log("You win! Paper beats Rock");
+        return 1;
+    }
+    else if(player == "Paper" && computerSelection == "Scissors")
+    {
+        console.log("You Lose! Scissors beats Paper");
+        return 2;
+    }
+    else if((player== "Rock" && computerSelection == "Rock") || (player== "Scissors" && computerSelection == "Scissors") || (player== "Paper" && computerSelection == "Paper"))
+        {
+            console.log("It's a Draw!");
+            return 3;
+        }
+    else console.log("Something went wrong.");
+}
+
+function game()
+{
+    let user;
+    for (let i = 0; i < 5; i++) {
+        user = playerChoose();
+        playRound(user, getComputerChoice());
+     }
 }
